a46cd03c0bf8f0caa7b001776a1a37e691e889e4
isadia-brian <developerisadia@gmail.com>
2023-08-22 06:59:20 +0300

    added api folder

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,226 +1,297 @@
 "use client";
 
 import Link from "next/link";
-
-import { useState, useEffect } from "react";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-  const [name, setName] = useState("");
-  const [studentNumber, setStudentNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [mobile, setMobile] = useState("");
-  const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [disabled, setDisabled] = useState(true);
-  const [nameError, setNameError] = useState(false);
-  const [usernameError, setUserNameError] = useState(false);
-  const [studentNumberError, setStudentNumberError] = useState(false);
-  const [emailError, setEmailError] = useState(false);
-  const [mobileError, setMobileError] = useState(false);
-  const [passwordError, setPasswordError] = useState(false);
-  const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+  const {
+    handleSubmit,
+    register,
+    watch,
+    getValues,
+    formState: { errors },
+  } = useForm();
 
-  const handleDisabled = () => {
-    const details = {
-      name,
-      studentNumber: studentNumber.toUpperCase(),
-      email,
-      mobile,
-      username,
-      password,
-      confirmPassword,
-    };
-
-    if (
-      details.name === "" ||
-      details.studentNumber === "" ||
-      details.email === "" ||
-      details.mobile === "" ||
-      details.username === "" ||
-      details.password === "" ||
-      details.confirmPassword === ""
-    ) {
-      setDisabled(true);
-    } else {
-      setDisabled(false);
-    }
+  const onSubmit = (data) => {
+    console.log(data);
+    console.log("data posted");
   };
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-
-    if (password != 1234) {
-      setPasswordError(true);
-      setDisabled(true);
-      return;
-    } else if (password !== confirmPassword) {
-      console.log("password is not equal");
-      setPasswordError(false);
-      setConfirmPasswordError(true);
-      setConfirmPassword("");
-      setDisabled(true);
-      return;
-    } else {
-      setPasswordError(false);
-      setConfirmPasswordError(false);
-      console.log("passwords match");
-    }
-  };
-
-  useEffect(() => {
-    const runDisabled = () => {
-      handleDisabled();
-    };
-    runDisabled();
-  }, [name, studentNumber, username, email, password, mobile, confirmPassword]);
-
   return (
     <div>
-      <div className="flex items-center">
-        <div></div>
-        <div className="h-screen px-4  w-full">
+      <div className="flex items-center md:flex-row-reverse">
+        <div className="hidden md:flex h-screen w-[50%]">
+          <div className="relative h-full w-full">
+            <Image
+              src="/images/image.jpg"
+              alt="student image"
+              fill
+              className="object-cover"
+            />
+          </div>
+        </div>
+        <div className="h-full px-4 md:px-24 w-full  md:w-[50%]">
           <div className="pt-10">
-            <h5 className="text-[19px] font-medium">Let's Get Started</h5>
-            <p className="text-[11px] font-light text-gray-400">
+            <h5 className="text-[19px] font-medium md:text-[32px] md:font-black">
+              Let's Get Started
+            </h5>
+            <p className="text-[11px] font-light text-gray-400 md:text-sm">
               Fill the form to continue
             </p>
           </div>
-          <div className="h-screen flex flex-col justify-center -mt-16  w-full">
-            <form className=" flex flex-col" onSubmit={handleFormSubmit}>
+          <div className="h-full flex flex-col  mt-9   w-full">
+            <form
+              className="h-full flex flex-col"
+              onSubmit={handleSubmit(onSubmit)}
+            >
               <div className="flex flex-col space-y-1 mb-6 w-full">
                 <label htmlFor="name" className="font-medium text-sm">
                   Full Name
                 </label>
                 <input
                   type="text"
-                  className="w-full text-sm outline-none border border-green-300/75 px-4 py-3 rounded-xl placeholder:text-[9px]"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
+                  className="w-full text-sm outline-none border border-slate-300 px-4 py-3 rounded-xl placeholder:text-[9px]"
                   placeholder="John Doe"
+                  {...register("name", {
+                    required: "true",
+                    minLength: 4,
+                    maxLength: 50,
+                  })}
                 />
+                {errors.name?.type === "required" ? (
+                  <>
+                    <span className="text-xs text-red-500">
+                      Name is required
+                    </span>
+                  </>
+                ) : errors.name?.type === "minLength" ? (
+                  <>
+                    <span className="text-xs text-red-500">
+                      Minimum characters should be 4 characters
+                    </span>
+                  </>
+                ) : errors.name?.type === "maxLength" ? (
+                  <>
+                    <span className="text-xs text-red-500">
+                      Maximum characters should be 50 characters
+                    </span>
+                  </>
+                ) : (
+                  <></>
+                )}
               </div>
 
               <div className="flex flex-col space-y-1 mb-6 w-full">
                 <label htmlFor="studentNumber" className="font-medium text-sm">
-                  Student Number
+                  Student ID
                 </label>
                 <input
                   type="text"
-                  className="w-full text-sm outline-none border border-green-300/75 px-4 py-3 rounded-xl placeholder:text-[9px]"
-                  value={studentNumber}
-                  onChange={(e) => setStudentNumber(e.target.value)}
+                  className="w-full text-sm outline-none border border-slate-300 px-4 py-3 rounded-xl placeholder:text-[9px]"
                   placeholder="KFS-222-444-555-666"
+                  {...register("studentNumber", {
+                    required: "true",
+                    pattern: /^kfs-2023-\d{3}-\d{3}-\d{3}$/,
+                  })}
                 />
+                {errors.studentNumber?.type === "required" ? (
+                  <>
+                    <span className="text-xs text-red-500">ID is required</span>
+                  </>
+                ) : errors.studentNumber?.type === "pattern" ? (
+                  <>
+                    <span className="text-xs text-red-500">
+                      This is not a valid student ID
+                    </span>
+                  </>
+                ) : (
+                  <></>
+                )}
               </div>
-              <div className="flex flex-col space-y-1 mb-6 w-full">
-                <label htmlFor="username" className="font-medium text-sm">
-                  Username
+
+              <div className="flex flex-col space-y-1 mb-6  w-full">
+                <label htmlFor="email" className="font-medium text-sm">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="w-full text-sm outline-none border border-slate-300 px-4 py-3 rounded-xl placeholder:text-[9px]"
+                  placeholder="johndoe@gmail.com"
+                  {...register("email", {
+                    required: "true",
+                    pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  })}
+                />
+                {errors.email?.type === "required" ? (
+                  <>
+                    <span className="text-xs text-red-500">
+                      Email is required
+                    </span>
+                  </>
+                ) : errors.email?.type === "pattern" ? (
+                  <>
+                    <span className="text-xs text-red-500">
+                      Enter a correct email address
+                    </span>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </div>
+
+              <div className="flex flex-col space-y-1 mb-6  w-full">
+                <label htmlFor="mobile" className="font-medium text-sm">
+                  Mobile No.
                 </label>
                 <input
                   type="text"
-                  className="w-full text-sm outline-none border border-green-300/75 px-4 py-3 rounded-xl placeholder:text-[9px]"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  placeholder="johnny"
+                  className="w-full text-sm outline-none border border-slate-300 px-4 py-3 rounded-xl placeholder:text-[9px]"
+                  placeholder="+2547......"
+                  {...register("mobile", {
+                    required: "true",
+                    pattern: /^(?:\+254|0)([1-9]\d{8})$/,
+                  })}
                 />
+                {errors.mobile?.type === "required" ? (
+                  <>
+                    <span className="text-xs text-red-500">
+                      Mobile Number is required
+                    </span>
+                  </>
+                ) : errors.mobile?.type === "pattern" ? (
+                  <>
+                    <span className="text-xs text-red-500">
+                      Enter a correct phone number
+                    </span>
+                  </>
+                ) : (
+                  <></>
+                )}
               </div>
-              <div className="flex items-center w-full mb-6 space-x-1">
-                <div className="flex flex-col space-y-1  w-full">
-                  <label htmlFor="email" className="font-medium text-sm">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    className="w-full text-sm outline-none border border-green-300/75 px-4 py-3 rounded-xl placeholder:text-[9px]"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="johndoe@gmail.com"
-                  />
-                </div>
-                <div className="flex flex-col space-y-1  w-full">
-                  <label htmlFor="mobile" className="font-medium text-sm">
-                    Mobile No.
-                  </label>
-                  <input
-                    type="number"
-                    className="w-full text-sm outline-none border border-green-300/75 px-4 py-3 rounded-xl placeholder:text-[9px]"
-                    value={mobile}
-                    onChange={(e) => setMobile(e.target.value)}
-                    placeholder="+2547......"
-                  />
-                </div>
-              </div>
-              <div className="flex items-center w-full space-x-1 mb-9">
-                <div className="flex flex-col space-y-1  w-full">
+
+              <div className="hidden md:flex md:space-x-2 ">
+                <div className="flex flex-col space-y-1 mb-6  w-full">
                   <label htmlFor="password" className="font-medium text-sm">
                     Password
                   </label>
-                  <div className="flex flex-col space-y-1 relative">
+                  <div className="flex flex-col space-y-1">
                     <input
                       type="password"
-                      className={`w-full text-sm outline-none  px-4 py-3 rounded-xl placeholder:text-[9px] ${
-                        passwordError
-                          ? "border border-red-500 placeholder:text-red-500"
-                          : "border border-green-300/75"
-                      }`}
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
+                      className={`w-full text-sm outline-none border border-slate-300  px-4 py-3 rounded-xl placeholder:text-[9px]`}
                       placeholder="********"
+                      {...register("password", {
+                        required: true,
+                        pattern: /^(?=.*[A-Z])(?=.*\d).{8,20}$/,
+                      })}
                     />
-                    {passwordError && (
+                    {errors.password?.type === "required" ? (
+                      <span className="text-xs text-red-500">
+                        Password is required
+                      </span>
+                    ) : errors.password?.type === "pattern" ? (
                       <>
-                        <p className="text-[10px] text-red-500 absolute -bottom-8">
-                          Password must be more than 8 characters
-                        </p>
+                        <span className="text-xs text-red-500 ">
+                          Password should contain an uppercase letter, a number
+                          and a should be between 8-12 characters
+                        </span>
                       </>
+                    ) : (
+                      <></>
                     )}
                   </div>
                 </div>
-                <div className="flex flex-col space-y-1  w-full">
+                <div className="flex flex-col space-y-1 mb-6  w-full">
                   <label
                     htmlFor="confirmPassword"
                     className="font-medium text-sm"
                   >
                     Confirm Password
                   </label>
-                  <div className="flex flex-col space-y-1 relative">
+                  <div className="flex flex-col space-y-1">
                     <input
                       type="password"
-                      className={`w-full text-sm outline-none  px-4 py-3 rounded-xl placeholder:text-[9px] ${
-                        confirmPasswordError
-                          ? "border border-red-500 placeholder:text-red-500"
-                          : "border border-green-300/75"
-                      }`}
-                      value={confirmPassword}
-                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      className={`w-full text-sm outline-none border border-slate-300  px-4 py-3 rounded-xl placeholder:text-[9px]`}
                       placeholder="********"
+                      {...register("confirmPassword", {
+                        required: "true",
+
+                        validate: (value) => value === getValues("password"),
+                      })}
                     />
-                    {confirmPasswordError && (
-                      <>
-                        <p className="text-[10px] text-red-500 absolute -bottom-4">
-                          Passwords do not match
-                        </p>
-                      </>
+                    {errors.confirmPassword && (
+                      <span className="text-xs text-red-500">
+                        Passwords don't match
+                      </span>
                     )}
                   </div>
+                </div>
+              </div>
+              <div className="md:hidden flex flex-col space-y-1 mb-6  w-full">
+                <label htmlFor="password" className="font-medium text-sm">
+                  Password
+                </label>
+                <div className="flex flex-col space-y-1">
+                  <input
+                    type="password"
+                    className={`w-full text-sm outline-none border border-slate-300  px-4 py-3 rounded-xl placeholder:text-[9px]`}
+                    placeholder="********"
+                    {...register("password", {
+                      required: true,
+                      pattern: /^(?=.*[A-Z])(?=.*\d).{8,20}$/,
+                    })}
+                  />
+                  {errors.password?.type === "required" ? (
+                    <span className="text-xs text-red-500">
+                      Password is required
+                    </span>
+                  ) : errors.password?.type === "pattern" ? (
+                    <>
+                      <span className="text-xs text-red-500 ">
+                        Password should contain an uppercase letter, a number
+                        and a should be between 8-12 characters
+                      </span>
+                    </>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              </div>
+              <div className="md:hidden flex flex-col space-y-1 mb-6  w-full">
+                <label
+                  htmlFor="confirmPassword"
+                  className="font-medium text-sm"
+                >
+                  Confirm Password
+                </label>
+                <div className="flex flex-col space-y-1">
+                  <input
+                    type="password"
+                    className={`w-full text-sm outline-none border border-slate-300  px-4 py-3 rounded-xl placeholder:text-[9px]`}
+                    placeholder="********"
+                    {...register("confirmPassword", {
+                      required: "true",
+
+                      validate: (value) => value === getValues("password"),
+                    })}
+                  />
+                  {errors.confirmPassword && (
+                    <span className="text-xs text-red-500">
+                      Passwords don't match
+                    </span>
+                  )}
                 </div>
               </div>
 
               <div className="w-full ">
                 <button
                   type="submit"
-                  className={`outline-none text-sm  py-4 rounded-xl w-full transition-colors duration-200 ease-linear ${
-                    disabled
-                      ? "bg-slate-400/50 text-slate-50 cursor-not-allowed "
-                      : "bg-black text-white"
-                  }`}
+                  className={`outline-none text-sm mb-6  py-4 bg-black text-white rounded-xl w-full transition-colors duration-200 ease-linear `}
                 >
                   Sign Up
                 </button>
               </div>
             </form>
-            <div className="w-full text-center mt-12">
+            <div className="w-full text-center mb-6">
               <p className="text-[11px] text-slate-500">
                 Already have an account?
                 <span>
